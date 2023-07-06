321e17ed4fc4937fa4171b375d2251010c1d1441
Viktor Samuel Podhradský <samkopodhradsky@gmail.com>
2023-07-06 12:55:56 +0200

    Add comments into script.js

--- public/script.js
+++ public/script.js
@@ -1,32 +1,40 @@
+// Selecting elements
 var openMenu = document.querySelector(".openMenu");
 var crossMenu = document.querySelector(".crossMenu");
 var mobileNav = document.querySelector(".mobileNav");
 var mobileMenuOptions = document.querySelectorAll(".mobileMenuOptions");
 
+// Event listener for opening the mobile navigation menu
 openMenu.addEventListener("click", function(event) {
   mobileNav.style.visibility = "visible";
 });
 
+// Event listener for closing the mobile navigation menu
 crossMenu.addEventListener("click", function(event) {
   mobileNav.style.visibility = "hidden";
 });
 
+// Event listeners for each mobile menu option to close the menu when clicked
 mobileMenuOptions.forEach(function(element) {
   element.addEventListener("click", function(event) {
     mobileNav.style.visibility = "hidden";
   });
 });
 
+// Checking if the 'usButton' exists on the page
 var usButton = document.querySelector(".moblieAboutButton");
 
 if (usButton) {
+  // Selecting additional elements related to the 'usButton'
   var usIcon = document.querySelector(".mobileAboutIcon");
   var activeIcon = "m";
   var usMenu = document.querySelector(".moblieAboutOptions");
   var radius = usIcon.style.borderTopLeftRadius;
 
+  // Event listener for toggling the 'usMenu' and changing the 'usIcon' based on its state
   usButton.addEventListener("click", function() {
     if (activeIcon == "m") {
+      // Changing to the close menu icon and modifying styles
       usIcon.src = "img/crossMenu.png";
       activeIcon = "c";
       usMenu.style.display = "block";
@@ -35,6 +43,7 @@ if (usButton) {
       usButton.style.borderBottomLeftRadius = "0";
       usButton.style.borderBottomRightRadius = "0";
     } else {
+      // Changing back to the menu icon and restoring styles
       usIcon.src = "img/menuIcon.png";
       activeIcon = "m";
       usMenu.style.display = "none";
